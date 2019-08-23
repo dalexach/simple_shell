884ca00f4d2c6056457d5228855bbaa62ebd71bd
dalexach <dalexach@gmail.com>
2019-08-23 19:38:48 +0000

    Ading special options to print some errors

--- error_messages.c
+++ error_messages.c
@@ -19,13 +19,22 @@ void fork_fail(void)
  * Return: Nothing.
 */
 
-void message_error(char **av, char *command, int count)
+void message_error(char **av, char *command, int count, int flag)
 {
 	write(STDERR_FILENO, command, _strlen(command));
 	write(STDERR_FILENO, ": ", 2);
 	_putchar(count);
 	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, av[0], _strlen(av[0]));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, "not found\n", 10);
+	if (flag == 2)
+	{
+		write(STDERR_FILENO, av[0], _strlen(av[0]));
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, "not found\n", 10);
+	}
+	if (flag == 1)
+	{
+		write(STDERR_FILENO, "Ilegal number: ", 18);
+		write(STDERR_FILENO, av[1], _strlen(av[1]));
+		write(STDERR_FILENO, "\n", 1);
+	}
 }
